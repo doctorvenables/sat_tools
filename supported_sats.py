167f8033602547e3a12ca430369320cea921ca63
DoctorVenables <doctorvenables@gmail.com>
2020-07-26 18:23:53 +0100

    Added a way of printing all currently supported satellites in gr-satellites to txt file in user's home

---
+++ supported_sats.py
@@ -0,0 +1,23 @@
+import os
+import yaml
+
+satyamlPath = '/usr/local/lib/python3/dist-packages/satellites/satyaml/'
+requiredData = ["norad","name","data"]
+outputFile = os.path.expanduser("~")+'/supported_satellites.txt'
+for filename in os.listdir(satyamlPath):
+  with open(outputFile, "a+") as output:
+    if filename.endswith('.yml'):
+#      print(filename)
+      file_path=satyamlPath+filename
+      with open(file_path) as file:
+        yaml_data = yaml.load(file)
+        str_to_print=''
+        for key in requiredData:
+          keyData = str(yaml_data[key])
+#        print(key + " : " + keyData)
+          str_to_print = str_to_print + keyData + ","
+#      continue
+      output.write(str_to_print+"\n")
+      continue
+    else:
+      continue
